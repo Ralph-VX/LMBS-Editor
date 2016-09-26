251b574a9f0ea3d8d21d624a6778cb15980f40cc
Ralph-VX <feizhaodan@hotmail.com>
2016-09-26 23:22:25 +0900

    Supported generating required characterList.json

--- src/kien/lmbseditor/core/CharacterList.java
+++ src/kien/lmbseditor/core/CharacterList.java
@@ -1,6 +1,13 @@
 package kien.lmbseditor.core;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+
+import net.arnx.jsonic.JSON;
+import net.arnx.jsonic.JSONException;
 
 public class CharacterList {
 	public LinkedHashMap<String, CharacterSet> lists;
@@ -22,11 +29,29 @@ public class CharacterList {
 		for (CharacterSet chara : lists.values()) {
 			chara.save();
 		}
+		saveList();
 	}
 
 	public void forceSave() {
 		for (CharacterSet chara : lists.values()) {
 			chara.forceSave();
+		}
+		saveList();
+	}
+	
+	private void saveList() {
+		LinkedHashMap<String, ArrayList<String>> list = new LinkedHashMap<String, ArrayList<String>>();
+		for (String charName : lists.keySet()) {
+			ArrayList<String> poses = new ArrayList<String>();
+			CharacterSet set = lists.get(charName);
+			poses.addAll(set.poses.keySet());
+			list.put(charName, poses);
+		}
+		try {
+			JSON.encode(list, new FileWriter(new File(EditorProperty.projectDirectory + "\\data\\characterList.json")), true);
+		} catch (JSONException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
