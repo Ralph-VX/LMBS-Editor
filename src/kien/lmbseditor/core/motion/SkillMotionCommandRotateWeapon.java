e30128d1923dc008947da1b659f989135219a915
Ralph-VX <feizhaodan@hotmail.com>
2017-07-18 21:59:08 +0900

    fix command string connected

--- src/kien/lmbseditor/core/motion/SkillMotionCommandRotateWeapon.java
+++ src/kien/lmbseditor/core/motion/SkillMotionCommandRotateWeapon.java
@@ -58,7 +58,7 @@ public class SkillMotionCommandRotateWeapon extends SkillMotionCommandBase {
 		return super.obtainCommandRepresentation() + 
 				": " + "Target: " + rotation + " degree, " + 
 				dur + " Frames, Inverted: " + (dir > 0 ? "true" : "false") + 
-				(rounds > 0 ? "for " + rounds + " rounds" : "" );
+				(rounds > 0 ? " for " + rounds + " rounds" : "" );
 	}
 
 	@Override
