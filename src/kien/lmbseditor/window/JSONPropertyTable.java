607e4e8ba55f71329dbcc8ef7806fcb24d85f123
Ralph-VX <feizhaodan@hotmail.com>
2018-03-19 17:26:32 +0900

    Now motion property will automatically start to edit first property.

--- src/kien/lmbseditor/window/JSONPropertyTable.java
+++ src/kien/lmbseditor/window/JSONPropertyTable.java
@@ -64,6 +64,10 @@ public class JSONPropertyTable extends JTable {
 				row++;
 			}
 			this.revalidate();
+			if (row >= 1) {
+				this.editCellAt(0, 1);
+				this.changeSelection(0, 1, false, false);
+			}
 		}
 	}
 
